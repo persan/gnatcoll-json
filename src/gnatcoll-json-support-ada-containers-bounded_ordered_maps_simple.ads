2cb8bd9e4032a69573e73bb03235cfb4ef3b487a
persan <per.s.sandberg@bahnhof.se>
2016-05-30 18:52:11 +0200

    Added Simple format for MAPS

---
+++ src/gnatcoll-json-support-ada-containers-bounded_ordered_maps_simple.ads
@@ -0,0 +1,19 @@
+
+with Ada.Containers.Bounded_Ordered_Maps;
+generic
+   with package M is new Standard.Ada.Containers.Bounded_Ordered_Maps (<>);
+   use M;
+   with function Image (Val : Key_Type) return String is <>;
+   with function Value (Val : String) return  Key_Type is <>;
+   with function Create (Val : Element_Type) return JSON_Value is <>;
+   with function Get (Val : JSON_Value) return Element_Type is <>;
+
+package GNATCOLL.JSON.Support.Ada.Containers.Bounded_Ordered_Maps_Simple is
+
+   function Create (Val : Map) return JSON_Value;
+   function Get (Val : JSON_Value) return Map;
+
+   function Get (Val : JSON_Value; Field : UTF8_String) return Map;
+   procedure Set_Field  (Val : JSON_Value;  Field_Name : UTF8_String; Field  : Map);
+
+end GNATCOLL.JSON.Support.Ada.Containers.Bounded_Ordered_Maps_Simple;
