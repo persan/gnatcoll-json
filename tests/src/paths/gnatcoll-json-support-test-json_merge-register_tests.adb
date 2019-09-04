e30f033000dd695d73dd55ae953b62887db07786
persan <per.s.sandberg@bahnhof.se>
2019-09-04 08:21:55 +0200

    New testcases for merge

---
+++ tests/src/paths/gnatcoll-json-support-test-json_merge-register_tests.adb
@@ -0,0 +1,18 @@
+--  begin read only
+separate (GNATCOLL.JSON.Support.Test.JSON_Merge)
+overriding procedure Register_Tests (Test : in out Test_Case) is
+   use AUnit.Test_Cases.Registration;
+begin
+   Register_Routine (Test, Test_Get_deep'Unrestricted_Access, "Test_Get_deep");
+   Register_Routine (Test, Test_Get_Float'Unrestricted_Access, "Test_Get_Float");
+   Register_Routine (Test, Test_Get_Integer'Unrestricted_Access, "Test_Get_Integer");
+   Register_Routine (Test, Test_Get_String'Unrestricted_Access, "Test_Get_String");
+   Register_Routine (Test, Test_Get_Array_1'Unrestricted_Access, "Test_Get_Array_1");
+   Register_Routine (Test, Test_Get_Array_2'Unrestricted_Access, "Test_Get_Array_2");
+   Register_Routine (Test, Test_Get_Array_3'Unrestricted_Access, "Test_Get_Array_3");
+   Register_Routine (Test, Test_Get_Array_Hidden_Alternate_1'Unrestricted_Access, "Test_Get_Array_Hidden_Alternate_1");
+   Register_Routine (Test, Test_Get_Array_Hidden_Alternate_2'Unrestricted_Access, "Test_Get_Array_Hidden_Alternate_2");
+   Register_Routine (Test, Test_Get_Array_Hidden_Alternate_3'Unrestricted_Access, "Test_Get_Array_Hidden_Alternate_3");
+   Register_Routine (Test, Fool'Unrestricted_Access, "Fool");
+end Register_Tests;
+--  end read only
---
+++ tests/src/paths/gnatcoll-json-support-test-json_merge.adb
@@ -0,0 +1,156 @@
+pragma Ada_2012;
+with GNAT;
+with GNAT.Source_Info;
+with AUnit; use AUnit;
+with GNATCOLL.JSON.Support;
+with AUnit.Assertions;
+package body GNATCOLL.JSON.Support.Test.JSON_Merge is
+   use AUnit.Assertions;
+   Unit_Name : constant String := GNAT.Source_Info.Enclosing_Entity;
+
+   -----------------
+   -- Set_Up_Case --
+   -----------------
+   Src : constant String :=
+           "{" &
+           "   ""float""   : 2.0 " & "," &
+           "   ""integer"" : 1 " & "," &
+           "   ""string""  : ""one"" " & "," &
+           "   ""struct""  : {""f1"": ""f1""} " & "," &
+           "   ""deep1""   : {""deep2"": " &
+           "                    {"  &
+           "                        ""deep3""    : ""rock""," &
+           "                        ""deep31""   : ""sss""" &
+           "                    }"  &
+           "                 }," &
+           "   ""array""   : [ 1,2,3, [1,2,3,4], { ""False"": false, " &
+           "                                       ""True"" : true" &
+           "                                     }" &
+           "                 ]" &
+           "}";
+
+   overriding procedure Set_Up_Case (Test : in out Test_Case) is
+   begin
+      Test.Data := Read (Src);
+   end Set_Up_Case;
+
+   procedure Test_Get_deep (Test : in out AUnit.Test_Cases.Test_Case'Class) is
+      T        : Test_Case renames Test_Case (Test);
+      Expected : constant String := "rock";
+      Result   : String (Expected'Range);
+   begin
+      Result := Get (GNATCOLL.JSON.Support.Get_Path (T.Data, "deep1.deep2.deep3"));
+      Assert (Result = Expected, "Got :" & Result & " Expected:" & Expected);
+   end Test_Get_deep;
+
+   procedure Test_Get_Float (Test : in out AUnit.Test_Cases.Test_Case'Class) is
+      T        : Test_Case renames Test_Case (Test);
+      Result   : Float;
+      Expected : constant := 2.0;
+   begin
+      Result := Get (GNATCOLL.JSON.Support.Get_Path (T.Data, "float"));
+      Assert (Result = Expected, "Got :" & Result'Img & " Expected:" & Expected'Img);
+   end Test_Get_Float;
+
+   procedure Test_Get_Integer (Test : in out AUnit.Test_Cases.Test_Case'Class) is
+      T        : Test_Case renames Test_Case (Test);
+      Result   : Integer;
+      Expected : constant := 1;
+   begin
+      Result := Get (GNATCOLL.JSON.Support.Get_Path (T.Data, "integer"));
+      Assert (Result = Expected, "Got :" & Result'Img & " Expected:" & Expected'Img);
+   end Test_Get_Integer;
+
+   procedure Test_Get_String (Test : in out AUnit.Test_Cases.Test_Case'Class) is
+      T        : Test_Case renames Test_Case (Test);
+      Expected : constant String := "one";
+      Result   : String (Expected'Range);
+   begin
+      Result := Get (GNATCOLL.JSON.Support.Get_Path (T.Data, "string"));
+      Assert (Result = Expected, "Got :" & Result & " Expected:" & Expected);
+   end Test_Get_String;
+
+   procedure Test_Get_Array_1 (Test : in out AUnit.Test_Cases.Test_Case'Class) is
+      T        : Test_Case renames Test_Case (Test);
+      Expected : constant Integer := 2;
+      Result   : Integer;
+   begin
+      Result := Get (GNATCOLL.JSON.Support.Get_Path (T.Data, "array(2)"));
+      Assert (Result = Expected, "Got :" & Result'Img & " Expected:" & Expected'Img);
+   end Test_Get_Array_1;
+
+   procedure Test_Get_Array_2 (Test : in out AUnit.Test_Cases.Test_Case'Class) is
+      T        : Test_Case renames Test_Case (Test);
+      Expected : constant Integer := 3;
+      Result   : Integer;
+   begin
+      Result := Get (GNATCOLL.JSON.Support.Get_Path (T.Data, "array(4)(3)"));
+      Assert (Result = Expected, "Got :" & Result'Img & " Expected:" & Expected'Img);
+   end Test_Get_Array_2;
+
+   procedure Test_Get_Array_3 (Test : in out AUnit.Test_Cases.Test_Case'Class) is
+      T        : Test_Case renames Test_Case (Test);
+      Expected : constant Boolean := True;
+      Result   : Boolean;
+   begin
+      Result := Get (GNATCOLL.JSON.Support.Get_Path (T.Data, "array(5).True"));
+      Assert (Result = Expected, "Got :" & Result'Img & " Expected:" & Expected'Img);
+   end Test_Get_Array_3;
+
+   ----------------------------------------------------------------------------
+   --  Note the tests for alternate arrays is a non documented feature
+   --  kept for compabilty with another implementation
+   --  This syntax is not to be documented in the public API.
+   --
+   procedure Test_Get_Array_Hidden_Alternate_1 (Test : in out AUnit.Test_Cases.Test_Case'Class) is
+      T        : Test_Case renames Test_Case (Test);
+      Expected : constant Integer := 2;
+      Result   : Integer;
+   begin
+      Result := Get (GNATCOLL.JSON.Support.Get_Path (T.Data, "array.2"));
+      Assert (Result = Expected, "Got :" & Result'Img & " Expected:" & Expected'Img);
+   end Test_Get_Array_Hidden_Alternate_1;
+
+   procedure Test_Get_Array_Hidden_Alternate_2 (Test : in out AUnit.Test_Cases.Test_Case'Class) is
+      T        : Test_Case renames Test_Case (Test);
+      Expected : constant Integer := 3;
+      Result   : Integer;
+   begin
+      Result := Get (GNATCOLL.JSON.Support.Get_Path (T.Data, "array.4.3"));
+      Assert (Result = Expected, "Got :" & Result'Img & " Expected:" & Expected'Img);
+   end Test_Get_Array_Hidden_Alternate_2;
+
+   procedure Test_Get_Array_Hidden_Alternate_3 (Test : in out AUnit.Test_Cases.Test_Case'Class) is
+      T        : Test_Case renames Test_Case (Test);
+      Expected : constant Boolean := True;
+      Result   : Boolean;
+   begin
+      Result := Get (GNATCOLL.JSON.Support.Get_Path (T.Data, "array.5.True"));
+      Assert (Result = Expected, "Got :" & Result'Img & " Expected:" & Expected'Img);
+   end Test_Get_Array_Hidden_Alternate_3;
+
+   procedure Fool (Test : in out AUnit.Test_Cases.Test_Case'Class) is
+      T        : Test_Case renames Test_Case (Test);
+      Expected : constant Boolean := True;
+      Result   : Boolean;
+   begin
+      Result := Get (GNATCOLL.JSON.Support.Get_Path (T.Data, "array.5.True"));
+      Assert (Result = Expected, "Got :" & Result'Img & " Expected:" & Expected'Img);
+   end Fool;
+   --------------------
+   -- Register_Tests --
+   --------------------
+
+   overriding procedure Register_Tests (Test : in out Test_Case) is separate;
+
+   ----------
+   -- Name --
+   ----------
+
+   overriding function Name (Test : Test_Case) return AUnit.Message_String is
+      pragma Unreferenced (Test);
+   begin
+      return Format (Unit_Name);
+   end Name;
+
+end GNATCOLL.JSON.Support.Test.JSON_Merge;
